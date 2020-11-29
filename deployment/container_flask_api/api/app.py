5ffa653aa7e5418884e8270c4925c20a40d76e78
Julian Merten <giuliano.merten@gmail.com>
2020-11-29 21:06:51 +0100

    Added tclap includes.

--- deployment/container_flask_api/api/app.py
+++ deployment/container_flask_api/api/app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-app = FLask(__name__)
+app = Flask(__name__)
 
 @app.route('/')
 def index():
     return "Hello World."
 
-if__name__ == '__main__':
+if __name__ == '__main__':
     app.run(debug=True)
