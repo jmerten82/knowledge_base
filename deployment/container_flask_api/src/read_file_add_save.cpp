42b44af796c85d913145c0b4a3f351d42afdc575
Julian Merten <julian.merten@daimler.com>
2020-12-21 14:14:32 +0000

    Started C++ routines.

--- deployment/container_flask_api/src/read_file_add_save.cpp
+++ deployment/container_flask_api/src/read_file_add_save.cpp
@@ -7,11 +7,15 @@ using namespace std;
 int main(int argc, char* argv[])
 {
     TCLAP::CmdLine cmd("Azure I/O", ' ', "0.1b");
-    TCLAP::ValueArg<std::string> containerArg("c","string","Container with file",true,"Something","string",cmd);
-    TCLAP::ValueArg<std::string> blobArg("b","string","Blob to download",true,"Something","string",cmd);
-    TCLAP::ValueArg<std::string> localArg("l","string","Local path to store file",true,"Something","string",cmd);
+    TCLAP::ValueArg<std::string> containerArg("c","container","Container with file",true,"RandomContainer","string",cmd);
+    TCLAP::ValueArg<std::string> blobArg("b","blob","Blob to download",true,"RandomBlob","string",cmd);
+    TCLAP::ValueArg<std::string> localArg("l","local","Local path to store file",true,"RandomFile","string",cmd);
     cmd.parse( argc, argv );
 
     string connection_string(getenv("AZURE_STRING"));
+    vector<string> connection_info = extract_connection_info(connection_string);
+
+
+    
     return 0;
 }
